db6e1840c3ad63957b6a54d1cbb6f0032321cb0b
shiang <shower085@gmail.com>
2020-09-09 21:45:38 +0800

    更新0909

---
+++ precache-manifest.816086a2b58c179e610edbdb1fcdda89.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ec6ea20efd820e52747",
+    "url": "web/css/app.353d4d03.css"
+  },
+  {
+    "revision": "1ec6ea20efd820e52747",
+    "url": "web/js/app.c20380b8.js"
+  },
+  {
+    "revision": "cb759e36bc7ef6c98cbe",
+    "url": "web/css/chunk-2938db00.072b71ab.css"
+  },
+  {
+    "revision": "cb759e36bc7ef6c98cbe",
+    "url": "web/js/chunk-2938db00.c807d602.js"
+  },
+  {
+    "revision": "162882667157c1084aff",
+    "url": "web/css/chunk-2e6c8973.ad9f0d23.css"
+  },
+  {
+    "revision": "162882667157c1084aff",
+    "url": "web/js/chunk-2e6c8973.9050bae7.js"
+  },
+  {
+    "revision": "d9d2cad83dd609c28786",
+    "url": "web/css/chunk-3181b741.755a5123.css"
+  },
+  {
+    "revision": "d9d2cad83dd609c28786",
+    "url": "web/js/chunk-3181b741.9989940e.js"
+  },
+  {
+    "revision": "b608772d1b9808bad1a0",
+    "url": "web/css/chunk-3e5ccedd.071a16af.css"
+  },
+  {
+    "revision": "b608772d1b9808bad1a0",
+    "url": "web/js/chunk-3e5ccedd.500e394c.js"
+  },
+  {
+    "revision": "36969eac7d827b6c67cc",
+    "url": "web/css/chunk-vendors.775402e4.css"
+  },
+  {
+    "revision": "36969eac7d827b6c67cc",
+    "url": "web/js/chunk-vendors.88e5953b.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "web/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "web/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "00a8726cd0ed0d646cad0f6916a7a58e",
+    "url": "index.html"
+  },
+  {
+    "revision": "fb0c32261c7dacb8041a64c096bb2edb",
+    "url": "img/login/logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "34819bdfa3598b784221c0b4fa47f49e",
+    "url": "img/avatar/avatar.png"
+  },
+  {
+    "revision": "8776b0483e67c6b13ad878a90054f998",
+    "url": "img/works/works04.png"
+  },
+  {
+    "revision": "c0bf73bd0fc1c1b7f6b3371dda0270bb",
+    "url": "img/works/works02.png"
+  },
+  {
+    "revision": "1db4e49ad83feaa0690de6b189d7cc95",
+    "url": "img/works/works01.png"
+  },
+  {
+    "revision": "fc11bd618e3cff856debea7e5fb29a1b",
+    "url": "img/works/works05.png"
+  },
+  {
+    "revision": "e5c423cac5cab6d2bc11078d955e15bd",
+    "url": "img/works/works03.png"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.816eda29798d08b8a8e36a9b71c887bf.js"
+  "precache-manifest.816086a2b58c179e610edbdb1fcdda89.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "prud"});
